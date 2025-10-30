02ead0638fd4db938d3c1fa41084f03158440291
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2025-10-30 08:28:03 -0400

    Add extra test files

--- test_d3.sh
+++ test_d3.sh
@@ -1,3 +1,6 @@
 python to_timeline_d3.py test_data/NCT04184622.json
 python to_timeline_d3.py test_data/NCT04557384.json
 python to_timeline_d3.py test_data/NCT03637764.json
+python to_timeline_d3.py test_data/NCT03421379.json
+python to_timeline_d3.py test_data/NCT04573309.json
+python to_timeline_d3.py test_data/NCT12345678.json
