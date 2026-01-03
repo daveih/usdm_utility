c9d256597854667cd80c604cb1e37ff3733edde2
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2026-01-03 14:48:21 +0100

    Add demo script

---
+++ demo_amend.sh
@@ -0,0 +1,4 @@
+python ./update_excel.py /Users/daveih/Documents/github/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/github/sdw_test/Other/xl_example.yaml
+python ./excel_diff.py /Users/daveih/Documents/github/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.xlsx
+python ./from_excel.py /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.xlsx
+python ./to_timeline.py /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.json
