fd6c12d6a44f191330fad65d9c0526aa83de3ff4
Dave Iberson-Hurst <daveih1664dk@gmail.com>
2026-01-03 17:11:08 +0100

    Update script to allow for a parameter

--- demo_amend.sh
+++ demo_amend.sh
@@ -1,4 +1,4 @@
-python ./update_excel.py /Users/daveih/Documents/github/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/github/sdw_test/Other/xl_example.yaml
-python ./excel_diff.py /Users/daveih/Documents/github/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.xlsx
-python ./from_excel.py /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.xlsx
-python ./to_timeline.py /Users/daveih/Documents/github/sdw_test/Other/xl_example_amended.json
+python ./update_excel.py /Users/daveih/Documents/$1/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/$1/sdw_test/Other/xl_example.yaml
+python ./excel_diff.py /Users/daveih/Documents/$1/sdw_test/Other/xl_example.xlsx /Users/daveih/Documents/$1/sdw_test/Other/xl_example_amended.xlsx
+python ./from_excel.py /Users/daveih/Documents/$1/sdw_test/Other/xl_example_amended.xlsx
+python ./to_timeline.py /Users/daveih/Documents/$1/sdw_test/Other/xl_example_amended.json
